4d0046cea3d88fb784894e73c0efee6217bd0e59
linojac <linojac@gmail.com>
2017-06-11 22:39:46 +0530

    adding shared components and user component

--- wedding-plan/src/app/app.module.ts
+++ wedding-plan/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {RoutingModule} from './routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TopNavPanelComponent } from './shared/top-nav-panel/top-nav-panel.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LoginPanelComponent } from './user/login-panel/login-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopNavPanelComponent,
+    FooterComponent,
+    LoginPanelComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
